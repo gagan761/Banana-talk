3b2212689a97f3386b2d0c117aeb7e9b6a5b595e
Gagan Kumar <gagan761@gmail.com>
2021-09-28 12:21:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+var inputText=document.querySelector("#translate-input");
+var outputText=document.querySelector("#translate-output");
+
+var url = "https://api.funtranslations.com/translate/minion.json" 
+
+function getTranslationUrl(input){
+    return url+"?"+"text="+ input;
+
+}
+
+
+function clickHandler(event){
+console.log(event);
+    var input= inputText.value;
+    fetch(getTranslationUrl(input))
+    .then(response=>response.json())
+    .then(json =>{
+        outputText.innerText= json.contents.translated;
+    })
+}
+
+
+function clearClickHandler(event){
+    
+inputText.value="";
+outputText.value="";
+
+}
+
+
+
+var translateButton= document.querySelector("#translate-button");
+var clearButton= document.querySelector("#clear-button");
+
+clearButton.addEventListener("click",clearClickHandler);
+
+translateButton.addEventListener("click",clickHandler);
